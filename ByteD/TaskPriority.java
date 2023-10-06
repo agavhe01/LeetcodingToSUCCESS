e9729bca6828a9ad81bc605e3c2c371f2c692635
Anesu Gavhera <agavhera@gmail.com>
2023-10-06 11:35:15 -0400

    byte avl

--- ByteD/TaskPriority.java
+++ ByteD/TaskPriority.java
@@ -1,35 +1,68 @@
-import java.util.PriorityQueue;
+import java.util.*;
+
 
 public class TaskPriority {
 
-    public static int maximumPrioritySum(int[] priority, int x, int y) {
-        int n = priority.length;
-        int[] lastTimePerformed = new int[n];
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+    public static int maximumPrioritySum(Integer[] priority, int x, int y) {
+        // Sort the tasks by priority in descending order.
+        Arrays.sort(priority, Collections.reverseOrder());
 
-        int maxSum = 0;
+        // Initialize a counter to track the current time.
+        int currentTime = 0;
 
-        for (int i = 0; i < y; i++) {
-            for (int j = 0; j < n; j++) {
-                if (lastTimePerformed[j] == 0 || i - lastTimePerformed[j] >= x) {
-                    maxHeap.offer(priority[j]);
-                    lastTimePerformed[j] = i;
-                    break; // Break to the next second
-                }
-            }
-            if (!maxHeap.isEmpty()) {
-                maxSum += maxHeap.poll();
-            }
+        // Initialize a set to track the available task types.
+        Set<Integer> availableTypes = new HashSet<>();
+        for (int i = 0; i < priority.length; i++) {
+            availableTypes.add(i);
         }
 
-        return maxSum;
+        // Initialize a map to track the last time a task of each type was performed.
+        Map<Integer, Integer> lastTimePerformed = new HashMap<>();
+
+        // Iterate over the tasks and perform the highest priority task that is available.
+        int totalPriority = 0;
+        while (currentTime < y) {
+            if (availableTypes.isEmpty()) {
+                break;
+            }
+
+            // Find the highest priority task type in the availableTypes set that is available to be performed.
+            int taskType = -1;
+            for (int type : availableTypes) {
+                if (currentTime - lastTimePerformed.getOrDefault(type, 0) >= x) {
+                    taskType = type;
+                    break;
+                }
+            }
+
+            // If no task type is available to be performed, break from the loop.
+            if (taskType == -1) {
+                break;
+            }
+
+            // Perform a task of that type.
+            totalPriority += priority[taskType];
+
+            // Update the last time a task of that type was performed.
+            lastTimePerformed.put(taskType, currentTime);
+
+            // Remove the task type from the availableTypes set.
+            availableTypes.remove(taskType);
+
+            // Update the current time.
+            currentTime++;
+        }
+
+        return totalPriority;
     }
 
     public static void main(String[] args) {
-        int[] priority = {3, 1, 2};
+        Integer[] priority = {3, 1, 2};
         int x = 5;
         int y = 7;
-        int result = maximumPrioritySum(priority, x, y);
-        System.out.println("Maximum Priority Sum: " + result); // Output: 11
+
+        int maxPrioritySum = maximumPrioritySum(priority, x, y);
+
+        System.out.println(maxPrioritySum);
     }
 }
