b479d39edf436a9ffb696da6563f0379318bbcf9
Anesu Gavhera <agavhera@gmail.com>
2023-10-11 14:33:40 -0400

    largest mountain

--- ByteD/largestMountain.java
+++ ByteD/largestMountain.java
@@ -1,28 +1,31 @@
 class Solution {
   public int longestMountain(int[] arr) {
     int ans = 0;
+        int i = 0;
 
-    for (int i = 0; i + 1 < arr.length;) {
-      while (i + 1 < arr.length && arr[i] == arr[i + 1])
-        ++i;
+        while (i + 1 < arr.length) {
+            while (i + 1 < arr.length && arr[i] == arr[i + 1]) {
+                ++i;
+            }
 
-      int increasing = 0;
-      int decreasing = 0;
+            int increasing = 0;
+            int decreasing = 0;
 
-      while (i + 1 < arr.length && arr[i] < arr[i + 1]) {
-        ++increasing;
-        ++i;
-      }
+            while (i + 1 < arr.length && arr[i] < arr[i + 1]) {
+                ++increasing;
+                ++i;
+            }
 
-      while (i + 1 < arr.length && arr[i] > arr[i + 1]) {
-        ++decreasing;
-        ++i;
-      }
+            while (i + 1 < arr.length && arr[i] > arr[i + 1]) {
+                ++decreasing;
+                ++i;
+            }
 
-      if (increasing > 0 && decreasing > 0)
-        ans = Math.max(ans, increasing + decreasing + 1);
-    }
+            if (increasing > 0 && decreasing > 0) {
+                ans = Math.max(ans, increasing + decreasing + 1);
+            }
+        }
 
-    return ans;
+        return ans;
   }
 }
